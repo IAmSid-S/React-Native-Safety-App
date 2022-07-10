dca84a5aa1f106141ce9927682ffd049b804073e
Siddharth Suryavanshi <siddharthsury02@outlook.com>
2022-07-10 21:12:04 +0530

    Added functionality to filter results based on location type

--- src/Presentation/Screens/SafeLocationListScreen.tsx
+++ src/Presentation/Screens/SafeLocationListScreen.tsx
@@ -1,30 +1,51 @@
+import { useEffect, useState } from 'react'
 import { View, Text, FlatList, ActivityIndicator } from 'react-native'
-import { List } from 'react-native-paper'
+import { Button, List } from 'react-native-paper'
 import { useSelector } from 'react-redux'
 import { RootState } from '../../Infrastructure/Store/store'
+import { SafeLocation } from '../../Types/Models/SafeLocation'
 import SafeLocationItem from '../Components/SafeLocation/SafeLocationItem'
+import Icon from '@expo/vector-icons/MaterialIcons';
 
 
 function SafeLocationListScreen(): JSX.Element {
 
     const safeLocations = useSelector((state: RootState) => state.SafeLocation.value)
+    const [selectedList, setSelectedList] = useState({ police_station: true, hospital: true });
+    const [filteredSafeLocations, setFilteredSafeLocations] = useState<SafeLocation[]>([]);
 
+    function toggleSelection(listType: keyof typeof selectedList) {
+        setSelectedList(prev => ({ ...prev, [listType]: !(prev[listType]) }))
+    }
+
+    useEffect(
+        () => {
+            let selectedKeys: SafeLocation['type'] = Object.keys(selectedList).filter(x => selectedList[x])
+            setFilteredSafeLocations(safeLocations.safeLocations.filter(x => selectedKeys.includes(x.type)))
+            console.log(safeLocations.safeLocations.filter(x => selectedKeys.includes(x.type)))
+        },
+        [safeLocations, selectedList]
+    );
     return (
-        <View style={{ flex: 1 }}>
+        <View style={{ flex: 1}}>
 
             {safeLocations.isLoading ? <ActivityIndicator size="large" /> :
-                <View style={{ height: 50, padding: 5 }}>
+                <View style={{ padding: 5 }}>
                     <Text style={{ textAlign: 'center', textAlignVertical: 'center' }}>
                         <Text style={{ fontSize: 15 }}>Safe Locations Found : </Text>
                         <Text style={{ fontSize: 25, fontWeight: 'bold' }}>    {safeLocations.safeLocations.length}</Text>
                     </Text>
+                    <View style={{ flexDirection: 'row', alignContent: 'space-around', justifyContent: 'center' }}>
+                        <Button onPress={() => toggleSelection('police_station')} style={{ borderRadius: 50, margin: 10, backgroundColor: (selectedList.police_station ? '#6200ee' : '#a075dd') }} mode='contained' icon='police-badge' ><Text style={{ fontSize: 10 }}>Police Stations : {safeLocations.safeLocations.filter(x => x.type === 'police_station').length}</Text></Button>
+                        <Button onPress={() => toggleSelection('hospital')} style={{borderRadius: 50, margin: 10, backgroundColor: (selectedList.hospital ? '#6200ee' : '#a075dd') }} mode='contained' icon='hospital'><Text style={{ fontSize: 10 }}>Hospitals : {safeLocations.safeLocations.filter(x => x.type === 'hospital').length}</Text></Button>
+                    </View>
                 </View>}
-            <View style={{ backgroundColor: '#e3e1e1', marginTop: 10, marginHorizontal: 2, borderWidth: 0.2 }}>
+            <View style={{ backgroundColor: '#e3e1e1', marginTop: 10, marginHorizontal: 2, borderWidth: 0.2, flex: 1 }}>
                 <List.AccordionGroup>
 
                     {safeLocations.error ? <Text style={{ color: 'red' }}>{safeLocations.error}</Text> :
                         <FlatList
-                            data={safeLocations.safeLocations}
+                            data={filteredSafeLocations}
                             renderItem={({ item }) => <SafeLocationItem {...item}></SafeLocationItem>}
                             keyExtractor={(item, index) => item.distance.toString()}
                         />
